e3721d8cbcc2d9623597627160324ba1562efc80
Mollycodd1e <selik7@mail.ru>
2022-02-21 17:28:06 +0300

    Добавление яндекс карты api

--- src/index.js
+++ src/index.js
@@ -1,3 +1,25 @@
 import '../src/sass/style.scss';
 
-console.log('привет');
+//import {YandexMap} from 'react-yandex-maps';
+//import React from 'react';
+//import ReactDOM from 'react-dom';
+//import {HashRouter} from 'react-router-dom';
+//import browserHistory from './browser-history.js';
+//import App from '../src/components/app/app.jsx';
+//import {configureStore} from '@reduxjs/toolkit';
+//import {Provider} from 'react-redux';
+//import reducer from './components/store/reducer.js';
+//import '../src/sass/style.scss';
+//
+//const store = configureStore({reducer: reducer});
+//
+//ReactDOM.render(
+//  <React.StrictMode>
+//    <Provider store={store}>
+//      <HashRouter history={browserHistory}>
+//        <App />
+//      </HashRouter>
+//    </Provider>
+//  </React.StrictMode>,
+//  document.getElementById('root')
+//);
